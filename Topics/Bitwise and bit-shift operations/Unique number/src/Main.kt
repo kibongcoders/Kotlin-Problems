a8ee49004c115418c24d6ffc9cb520842edcbdc6
kibongcorders <kkp02052@gmail.com>
2024-06-19 23:53:22 +0900

    study : variable

--- Topics/Bitwise and bit-shift operations/Unique number/src/Main.kt
+++ Topics/Bitwise and bit-shift operations/Unique number/src/Main.kt
@@ -1,4 +1,8 @@
 fun main() {
-    val (a, b, c) = readLine()!!.split(" ").map { it.toInt() }
+    val (a, b, c) = readLine()!!.split(" ").map {
+        it.toInt()
+    }
+
+    println(a xor b xor c)
     // write your code here
 }
