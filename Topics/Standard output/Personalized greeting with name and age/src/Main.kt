34823a15367460e2b126d234fc8e53cd79edfe30
kibongcorders <kkp02052@gmail.com>
2024-06-24 21:51:41 +0900

    study : function

---
+++ Topics/Standard output/Personalized greeting with name and age/src/Main.kt
@@ -0,0 +1,12 @@
+// Import the necessary libraries
+
+fun main() {
+    // Read the person's name
+    val name = readln()
+    val age = readln().toInt()
+
+    // Read the person's age
+    print("Hello $name! You are $age years old.")
+    // Print out the personalized greeting
+
+}
